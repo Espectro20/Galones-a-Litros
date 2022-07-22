7dcae1a92320f082ce87c61f84af9512c6d80dc0
Espectro20 <104633841+Espectro20@users.noreply.github.com>
2022-07-22 13:02:03 -0400

    Add files via upload

---
+++ Galones a Litros/Galones a Litros/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Galones_a_Litros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.dtgv = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Litros = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Galones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(140, 260);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 0;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(241, 260);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // dtgv
+            // 
+            this.dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Litros,
+            this.Galones,
+            this.Precio});
+            this.dtgv.Location = new System.Drawing.Point(12, 93);
+            this.dtgv.Name = "dtgv";
+            this.dtgv.Size = new System.Drawing.Size(352, 150);
+            this.dtgv.TabIndex = 2;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "ID";
+            this.Id.Name = "Id";
+            this.Id.Width = 50;
+            // 
+            // Litros
+            // 
+            this.Litros.HeaderText = "Litros/Km";
+            this.Litros.Name = "Litros";
+            this.Litros.Width = 80;
+            // 
+            // Galones
+            // 
+            this.Galones.HeaderText = "Galones";
+            this.Galones.Name = "Galones";
+            this.Galones.Width = 80;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            this.Precio.Width = 80;
+            // 
+            // cantidad
+            // 
+            this.cantidad.Location = new System.Drawing.Point(241, 56);
+            this.cantidad.Multiline = true;
+            this.cantidad.Name = "cantidad";
+            this.cantidad.Size = new System.Drawing.Size(53, 20);
+            this.cantidad.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Precio de la Gasolina: 293.6";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Introduzca la cantidad de Litros:";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCerrar.Location = new System.Drawing.Point(38, 260);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 6;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 309);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cantidad);
+            this.Controls.Add(this.dtgv);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpiar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DataGridView dtgv;
+        private System.Windows.Forms.TextBox cantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Litros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Galones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ Galones a Litros/Galones a Litros/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Galones_a_Litros
+{
+    public partial class Form1 : Form
+    {
+        public double a = 10.5;
+        public double cash = 293.6;
+        public double valor;
+        public double gal;
+        public int count = 0;
+        public int id = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double b = Convert.ToDouble(cantidad.Text);
+            if (b < a)
+            {
+                string message = "El valor introducido debe ser mayor a 10.4";
+                string caption = "Error";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                result = MessageBox.Show(message, caption, buttons);
+
+            }
+            while (a <= b)
+            {
+                gal = b * 0.264172;
+                valor = cash * gal;
+                int n = dtgv.Rows.Add();
+                valor = Math.Round(valor, 2);
+                gal = Math.Round(gal, 2);
+                dtgv.Rows[n].Cells[0].Value = id;
+                dtgv.Rows[n].Cells[1].Value = b + "/Km";
+                dtgv.Rows[n].Cells[2].Value = gal;
+                dtgv.Rows[n].Cells[3].Value = "DOP$ " + valor;
+                b = b - 0.1;
+                id++;
+            }
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            cantidad.Text = "";
+            dtgv.Rows.Clear();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
